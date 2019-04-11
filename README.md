a5e59cc87fb46c190f41548932f8bd825877e07d
turovtsov <dturovtsov@marathonbet.ru>
2019-04-11 15:20:12 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # empty-ssr
 Server Side Rendering
+
+## Server
+
+```bash
+npm i express
+npm i isomorphic-fetch
+npm i react-helmet
+npm i @babel/core
+npm i @babel/node
+```
+
+### Run Server
+
+```bash
+babel-node ./server/server.js
+```
---
+++ flow-typed/waves.js
@@ -0,0 +1,23 @@
+// @flow
+
+type WavesAuthDataType = {
+    +name: string,
+    +data: string,
+};
+
+type WavesAuthResultType = {
+    +address: string,
+    +data: string,
+    +host: string,
+    +name: string,
+    +prefix: string,
+    +publicKey: string,
+    +signature: string,
+};
+
+type WavesType = {
+    auth(WavesAuthDataType):Promise<WavesAuthResultType | Error>,
+};
+
+declare var Waves: WavesType;
+
